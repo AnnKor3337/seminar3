df0ba4ef6d855dceac4adeeb3941887a7982a98d
Anna <zharova1992@gmail.com>
2022-07-01 23:45:57 +0400

    решена задача 23

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/* РЕшение задачи 23
+
+
+void cub(int n)
+{
+    int i=0;
+    for (; i <= n; i++)
+    {
+        int num = i*i*i;
+        Console.WriteLine(i + "^3 = "+ num);
+    }
+}
+
+Console.WriteLine("Введите число N, для формирования таблицы кубов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+cub(n); */
